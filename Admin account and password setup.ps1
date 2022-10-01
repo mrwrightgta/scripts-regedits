f742604130b2345a23882c840ec9717b9de3a5dd
mrwrightgta <112006880+mrwrightgta@users.noreply.github.com>
2022-10-01 23:01:38 +0100

    Updating script

--- Admin account and password setup.ps1
+++ Admin account and password setup.ps1
@@ -1,13 +1,10 @@
     ## Admin Account Setup
-    {
-        Read-Host "Here we are going to set up the default admin account and give it a password of your choice"
+        Write-Host "Here we are going to set up the default admin account and give it a password of your choice"
 
-        $Password = Read-Host -AsSecureString "Please enter the password for the admin user, make it something you will remember"
+        $Password1 = Read-Host -AsSecureString "Please enter the password for the admin user, make it something you will remember"
+        $Password2 = Read-Host -AsSecureString "Please confirm your password"
 
         Get-LocalUser -Name "Administrator" | Enable-LocalUser
      
         $UserAccount = Get-LocalUser -Name "Administrator"
         $UserAccount | Set-LocalUser -Password $Password
-        Read-Host "Done! Press enter to go back to the Main Menu"
-        
-    }
